94ffe04b48ee20c47cd32a7a675ce872191c3708
Yubing Wan <yw@yubings-mbp.lan>
2024-01-22 23:43:55 -0500

    add more comparison and clean up code

--- R/funcs.R
+++ R/funcs.R
@@ -1,31 +1,58 @@
-fit_4pl_model <- function(df, log50_0 = -6, h_0 = -1, top_0 = 42, bottom_0 = 0, constr_top = NULL, constr_bottom = NULL, ...) {
+if (!require("drc", character.only = TRUE)) {
+    install.packages("drc", dependencies = TRUE)
+    library("drc", character.only = TRUE)
+}
+
+if (!require("broom", character.only = TRUE)) {
+    install.packages("broom", dependencies = TRUE)
+    library("broom", character.only = TRUE)
+}
+
+if (!require("reticulate", character.only = TRUE)) {
+    install.packages("reticulate", dependencies = TRUE)
+    library("reticulate", character.only = TRUE)
+}
+
+theme_set(theme_bw())
+
+fit_4pl_model <- function(
+    df, ec50 = 1e-6, h = -1, top = 42, bottom = 0,
+    constr_top = NULL, constr_bottom = NULL, constr_h = NULL, ...) {
 
   if(!is.null(constr_top) & is.null(constr_bottom)) {
     top <- constr_top
-    f1 <- response ~ bottom + (top - bottom)/(1 + 10^((logEC50 - log10(dose)) * h))
-    m1 <- nls(f1, data = df, start = list(logEC50 = log50_0, h = h_0, bottom = bottom_0), ...)
-    coeffs <- list(logEC50 = coef(m1)[1], h = coef(m1)[2], top = top, bottom = coef(m1)[3])
+    start_list <- list(ec50 = ec50, h = h, bottom = bottom)
+    f1 <- response ~ bottom + (top - bottom)/(1 + 10^((log10(ec50) - log10(dose)) * h))
+    m1 <- nls(f1, data = df, start = start_list, ...)
+    coeffs <- list(ec50 = coef(m1)[1], h = coef(m1)[2], top = top, bottom = coef(m1)[3])
   }
 
   if(is.null(constr_top) & !is.null(constr_bottom)) {
     bottom <- constr_bottom
-    f2 <- response ~ bottom + (top - bottom)/(1 + 10^((logEC50 - log10(dose)) * h))
-    m1 <- nls(f2, data = df, start = list(logEC50 = log50_0, h = h_0, top = top_0), ...)
-    coeffs <- list(logEC50 = coef(m1)[1], h = coef(m1)[2], top = coef(m1)[3], bottom = bottom)
+    f2 <- response ~ bottom + (top - bottom)/(1 + 10^((log10(ec50) - log10(dose)) * h))
+    m1 <- nls(f2, data = df, start = list(ec50 = ec50, h = h, top = top), ...)
+    coeffs <- list(ec50 = coef(m1)[1], h = coef(m1)[2], top = coef(m1)[3], bottom = bottom)
   }
 
   if(!is.null(constr_top) & !is.null(constr_bottom)) {
     top <- constr_top
     bottom <- constr_bottom
-    f3 <- response ~ bottom + (top - bottom)/(1 + 10^((logEC50 - log10(dose)) * h))
-    m1 <- nls(f3, data = df, start = list(logEC50 = log50_0, h = h_0), ...)
-    coeffs <- list(logEC50 = coef(m1)[1], h = coef(m1)[2], top = top, bottom  = bottom)
+    f3 <- response ~ bottom + (top - bottom)/(1 + 10^((log10(ec50) - log10(dose)) * h))
+    m1 <- nls(f3, data = df, start = list(ec50 = ec50, h = h), ...)
+    coeffs <- list(ec50 = coef(m1)[1], h = coef(m1)[2], top = top, bottom  = bottom)
   }
 
   if(is.null(constr_top) & is.null(constr_bottom)) {
-    f4 <- response ~ bottom + (top - bottom)/(1 + 10^((logEC50 - log10(dose)) * h))
-    m1 <- nls(f4, data = df, start = list(logEC50 = log50_0, h = h_0, top = top_0, bottom = bottom_0), ...)
-    coeffs <- list(logEC50 = coef(m1)[1], h = coef(m1)[2], top = coef(m1)[3], bottom  = coef(m1)[4])
+    f4 <- response ~ bottom + (top - bottom)/(1 + 10^((log10(ec50) - log10(dose)) * h))
+    m1 <- nls(f4, data = df, start = list(ec50 = ec50, h = h, top = top, bottom = bottom), ...)
+    coeffs <- list(ec50 = coef(m1)[1], h = coef(m1)[2], top = coef(m1)[3], bottom  = coef(m1)[4])
+  }
+
+  if(!is.null(constr_h)) {
+    h <- constr_h
+    f5 <- response ~ bottom + (top - bottom)/(1 + 10^((log10(ec50) - log10(dose)) * h))
+    m1 <- nls(f5, data = df, start = list(ec50 = ec50, top = top, bottom = bottom), ...)
+    coeffs <- list(ec50 = coef(m1)[1], h = h, top = coef(m1)[2], bottom  = coef(m1)[3])
   }
 
   coeffs
@@ -33,9 +60,29 @@ fit_4pl_model <- function(df, log50_0 = -6, h_0 = -1, top_0 = 42, bottom_0 = 0, 
 
 plot_dose_response_curve <- function(c0, df, ...) {
   plot(x = df$dose, y = df$response, log = "x", xlab = "Dose (M)", ylab = "Response", ...)
-  X1 <- seq(-9, -2, 0.1) # X-data for the curve
-  y <- c0$bottom + (c0$top - c0$bottom)/(1 + 10^((c0$logEC50 - X1) * c0$h)) # Y data
-  lines(10^X1, y, col="blue", lwd=2, ...) # plot the curve
+  x <- 10^seq(-9, -2, 0.1)
+  y <- c0$bottom + (c0$top - c0$bottom)/(1 + 10^((log10(c0$ec50) - log10(x)) * c0$h))
+  lines(x, y, col="blue", lwd=2, ...)
+}
+
+plot_dose_response_with_fit <- function(
+    data, coef, title = "Dose-Response Curve", color = "blue", width = 8, height = 6) {
+
+  data <- data %>%
+    mutate(fitted = coef$bottom + (coef$top - coef$bottom)/(1 + 10^((log10(coef$ec50) - log10(dose)) * coef$h)))
+
+  p <- ggplot(data, aes(x = dose)) +
+    geom_point(aes(y = response), color = color, size = 3, show.legend = TRUE) +
+    geom_line(aes(y = fitted), color = color, show.legend = TRUE) +
+    ggtitle(title) +
+    xlab("Dose") +
+    ylab("Response") +
+    theme_minimal() +
+    theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5)) +
+    scale_color_manual(values = c("Observed Data" = color, "Fitted Curve" = color))
+
+  # Print the plot
+  print(p)
 }
 
 simulate_dose_response_data <- function(
---
+++ script/funcs.py
@@ -0,0 +1,54 @@
+import subprocess
+import pkg_resources
+required_packages = ["numpy", "scipy", "matplotlib"] 
+installed_packages = {pkg.key for pkg in pkg_resources.working_set}
+missing_packages = [pkg for pkg in required_packages if pkg not in installed_packages]
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+if missing_packages:
+    subprocess.check_call(["python", "-m", "pip", "install", *missing_packages])
+
+def four_parameter_logistic(x, ec50, hill_slope, top, bottom):
+    # return bottom + (top - bottom) / (1 + (x / ec50)**(-hill_slope))
+    return bottom + (top - bottom) / (1 + 10**((np.log10(ec50) - np.log10(dose)) * hill_slope))
+
+def fit_4pl_model_py(x_data, y_data, initial_guess=(50, 1, 100, 0)):
+    popt, _ = curve_fit(four_parameter_logistic, x_data, y_data, p0=initial_guess, maxfev=10000)
+    return popt
+  
+def calculate_fitted_response(dose, popt):
+    ec50, hill_slope, top, bottom = popt
+    fitted_response = four_parameter_logistic(dose, ec50, hill_slope, top, bottom)
+    return fitted_response
+
+def plot_dose_response_with_fit_py(dose, response, fitted_response, title="Dose-Response Curve", color = "blue"):
+    plt.figure(figsize=(8, 6))
+    plt.scatter(dose, response, color=color, label='Observed Data')  # Plot observed data points
+    plt.plot(dose, fitted_response, color=color, label='Fitted Curve')  # Plot fitted curve
+    plt.title(title)
+    plt.xlabel("Dose")
+    plt.ylabel("Response")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+def simulate_dose_response(
+  n=100, response_type='inhibition', ec50=50, hill_coeff=1, 
+  max_response=100, min_response=0, noise_level=0.05):
+    dose = np.linspace(min_response, max_response, n)
+
+    if response_type == 'inhibition':
+        response = max_response / (1 + (dose/ec50)**hill_coeff)
+    elif response_type == 'stimulation':
+        response = min_response + (max_response - min_response) * (dose**hill_coeff / (ec50**hill_coeff + dose**hill_coeff))
+    else:
+        raise ValueError("response_type must be 'inhibition' or 'stimulation'")
+
+    # Add random noise to simulate experimental variability
+    response += np.random.normal(0, noise_level * max_response, n)
+
+    return dose, response
+  
+  
